7e5e7f868512d27ed8005092ec21c55d308537a2
sjh <sjh@gmail.com>
2025-09-15 09:59:57 +0900

    커밋테스트

---
+++ src/Components/header.jsx
@@ -0,0 +1,12 @@
+import {useNavigate} from "react-router-dom"
+export default function Footer() {
+    const usenavigate = useNavigate();
+    return(
+        <div>
+            <div>
+                <span>MooDiary</span>
+            </div> 
+        </div>
+    )
+    
+};
