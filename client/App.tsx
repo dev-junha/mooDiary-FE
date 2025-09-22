38ac56ffc7bc3d6cff52129d5ab14d065c141252
sjh <sjh@gmail.com>
2025-09-22 23:06:00 +0900

    메인페이지 구현, 기능연결 미완성

---
+++ client/App.tsx
@@ -0,0 +1,43 @@
+import "./global.css";
+
+import { Toaster } from "@/components/ui/toaster";
+import { createRoot } from "react-dom/client";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
+import Layout from "@/components/layout/Layout";
+import WriteEdit from "@/pages/WriteEdit";
+import Results from "@/pages/Results";
+import Bookmark from "@/pages/Bookmark";
+import Profile from "@/pages/Profile";
+import Recommendation from "@/pages/Recommendation";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/write" element={<WriteEdit />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/bookmark" element={<Bookmark />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/recommendation" element={<Recommendation />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+createRoot(document.getElementById("root")!).render(<App />);
