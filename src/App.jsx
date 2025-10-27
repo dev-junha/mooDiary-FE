a2b1dccc15a0ac713a78f830b56fb004829a7131
walnut <2044lava@gmail.com>
2025-10-28 06:27:23 +0900

    [feat] 리액트-테일윈드 이식 및 더미 api 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-//-----------------------------------------------------
-import { Routes, Route, Link } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import DiaryNew from "./Pages/DiaryNew.jsx";
-import DiaryEdit from "./Pages/DiaryEdit.jsx";
+// src/App.jsx
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
 
-export default function App() {
+function App() {
   return (
-    <div className="app-wrap">
-      {/* <Header /> */}
-      <header className="app-header">
-        <Link to="/" className="brand">mooDiary</Link>
-        <nav>
-          <Link to="/diary/new" className="nav-btn">새 감정일기</Link>
-        </nav>
-      </header>
-
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<div>홈</div>} />
-          <Route path="/diary/new" element={<DiaryNew />} />
-          <Route path="/diary/:id/edit" element={<DiaryEdit />} />
-        </Routes>
+    // style.css의 body 스타일 적용
+    // min-h-screen: 100vh, bg-gradient-to-b: linear-gradient(180deg)
+    // from-light-bg-start/to-light-bg-end: config에 등록한 커스텀 색상
+    <div className="min-h-screen bg-gradient-to-b from-light-bg-start to-light-bg-end flex flex-col font-default">
+      <Header />
+      
+      {/* style.css의 .content 스타일 적용 */}
+      {/* flex-grow: 남은 공간 차지, max-w-7xl: 1280px (1440px와 근사) */}
+      {/* mx-auto: 중앙 정렬, px-[162px]: 임의 패딩값 */}
+      <main className="content flex-grow w-full max-w-7xl mx-auto px-5 md:px-20 lg:px-[162px] py-5 flex flex-col gap-10">
+        <HomePage />
       </main>
-
-      {/* <Footer /> */}
-      <Toaster position="top-center" />
+      
+      <Footer />
     </div>
-  );
+  )
 }
+
+export default App
