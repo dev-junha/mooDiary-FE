c93c32ca95edd29a30b77a82818fe7195fdad498
sjh <sjh@gmail.com>
2025-12-07 23:51:47 +0900

    배포 수정

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/Header.tsx";
 import { useNavigate } from "react-router-dom";
 import {
   getUserDiaries,
